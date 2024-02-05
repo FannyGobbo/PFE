bbfea4a5f57f11f258929e409fd912ad98d10c30
alanCrtl <alan.courtel1999@gmail.com>
2024-02-05 14:53:11 +0100

    refactoring

--- model_integration/wav_midi_pdf_pipeline.py
+++ model_integration/wav_midi_pdf_pipeline.py
@@ -21,7 +21,16 @@ USAGE: wav_midi_pdf_pipeline.py [-h] input_wav_path output_pdf_name
     -h, --help       show this help message and exit
 """
 
-def convert_midi_to_pdf(midi_file, pdf_file):
+def convert_wav_to_pdf(wav_file, pdf_file):
+	# same as terminal command: $ basic-pitch midi/ path/file.wav
+    wav_to_midi_process = ["basic-pitch", "./midi/", wav_file]
+    subprocess.run(wav_to_midi_process)
+
+    # midi file location, and pdf output file location
+    midi_file = "midi/" + wav_file.split("/")[-1].split(".")[0] + "_basic_pitch" + ".mid"
+    print(f"midi file location: {midi_file}")
+    pdf_file = "sheets/" + pdf_file
+
     # Convert MusicXML to PDF using MuseScore tool
     subprocess.run(["mscore3", midi_file, "-o", pdf_file])
 
@@ -36,14 +45,5 @@ if __name__ == "__main__":
     wav_file = args.input_wav_path
     pdf_file =  args.output_pdf_name
 
-	# same as terminal command: $ basic-pitch midi/ path/file.wav
-    wav_to_midi_process = ["basic-pitch", "./midi/", wav_file]
-    subprocess.run(wav_to_midi_process)
-
-    # midi file location, and pdf output file location
-    midi_file = "midi/" + wav_file.split("/")[-1].split(".")[0] + "_basic_pitch" + ".mid"
-    print(f"midi file location: {midi_file}")
-    pdf_file = "sheets/" + pdf_file
-
-    # Convert MIDI file to PDF
-    convert_midi_to_pdf(midi_file, pdf_file)
+    # Convert wav file to PDF
+    convert_wav_to_pdf(wav_file, pdf_file)
